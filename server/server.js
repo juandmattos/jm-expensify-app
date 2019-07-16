8526ce9ea09e82fef4f76df9ce4a6dd83f620839
Juan Diego Mattos <juandmattos@gmail.com>
2019-07-16 16:47:56 -0300

    Fix res.sendFile in server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000
 app.use(express.static(publicPath))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html')) // If what the user needs is not in the public folder, send index.html
+  res.sendFile(path.join(publicPath, 'index.html')) // If what the user needs is not in the public folder, send index.html
 })
 
 app.listen(port, () => {
