6db8fb9bf026f866cb70ae1fb7f91c37f0384816
Juan Diego Mattos <juandmattos@gmail.com>
2019-07-16 17:22:20 -0300

    Added format in ExpenseListItem

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -4,7 +4,7 @@ import { ExpenseListItem } from '../../components/ExpenseListItem'
 import expenses from '../fixtures/expenses'
 
 test('should render ExpenseListList with expense', () => {
-  const wrapper = shallow(<ExpenseListItem expense={expenses[0]}/>)
+  const wrapper = shallow(<ExpenseListItem expense={expenses[2]}/>)
 
   expect(wrapper).toMatchSnapshot()
 })
